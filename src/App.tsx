8006bcd8344ea27206332980bf491afa99094ec8
Ruipeng Han <ruipenghan@gmail.com>
2023-10-08 19:27:21 -0500

    Initialized ListView.

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,55 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import ListView from "./components/ListView";
+import GalleryView from "./components/GalleryView";
+import DetailView from "./components/DetailView";
 
-const App = () => {
-  return <div>App</div>;
+import axios from "axios";
+
+const App: React.FC = () => {
+  // const data = [
+  //   { id: 1, name: "Bitcoin", imageUrl: "...", description: "..." },
+  //   // ... other data
+  // ];
+  const [data, setData] = useState<any[]>([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const result = await axios.get(
+        "https://rest.coinapi.io/v1/assets/icons/32",
+        {
+          headers: {
+            "X-CoinAPI-Key": "10E86574-F2B3-4C3A-A9A9-A0C22E85E079",
+          },
+        }
+      );
+
+      setData(result.data); // Or adjust based on the structure of the API response
+    };
+
+    fetchData();
+  }, []);
+
+  // Just as a fallback if the API call hasn't completed yet
+  if (!data.length) return <div>Loading...</div>;
+
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<ListView data={data} />} />
+        <Route
+          path="/gallery"
+          element={
+            <GalleryView
+              onItemSelect={(item) => console.log(item)}
+              data={data}
+            />
+          }
+        />
+        <Route path="/detail/:itemId" element={<DetailView data={data} />} />
+      </Routes>
+    </Router>
+  );
 };
 
 export default App;
