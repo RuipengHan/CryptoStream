921cc0adecdb58799cfb7fe19412d5b317b39633
Ruipeng Han <ruipenghan@gmail.com>
2023-10-10 21:44:29 -0500

    Updated header and title.

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import styles from "./Header.module.css";
 
 const Header: React.FC = () => {
-  return <header className={styles.header}>Concurrent Crypto Status</header>;
+  return <header className={styles.header}>Crypto Streaming</header>;
 };
 
 export default Header;
